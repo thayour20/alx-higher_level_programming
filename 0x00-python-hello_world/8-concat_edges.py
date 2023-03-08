429ebdba89e599e1cc023123f7f4a99b1c3a6c7d
thayour20 <suleimanridwan@gmail.com>
2023-03-08 13:52:59 -0800

    conc2a

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(f"{str[39:67]}with python")
+ str = (str[39:66] + str[106:112] + str[0:6])
+print(str)
